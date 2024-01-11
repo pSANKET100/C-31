ecb9d7d43a39ddd2520ea6633324b75096c58917
Ritesh Chavan <riteshshivajichavan@gmail.com>
2024-01-11 07:50:25 +0530

    Tested php

---
+++ html/signup.php
@@ -0,0 +1,10 @@
+<?php
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirmpass = $_POST['confirm_password'];
+
+echo "Hello $username. Your email is $email. Your password is $password";
+
+?>
