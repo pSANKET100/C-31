74e7e22312fd0467a2c7b6c3f0374935964b6866
Ritesh Chavan <riteshshivajichavan@gmail.com>
2024-01-25 16:47:02 +0530

    UI for HomePage

--- signup.php
+++ signup.php
@@ -4,20 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="signup.css">
     <title>Document</title>
-    <style>
-        body {
-            margin: 0px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
 
-        .center-container {
-            text-align: center;
-        }
-    </style>
 </head>
 
 <body>
